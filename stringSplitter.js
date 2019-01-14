805e45698b961ff3b8dbf6ea75f968b868856079
Nikkie Mashian <nikkiemashian@gmail.com>
2019-01-14 10:24:51 -0800

    another solution for string splitter

--- stringSplitter.js
+++ stringSplitter.js
@@ -28,3 +28,16 @@ function stringSplitter(str, seperator, index){
 }
 
 console.log(stringSplitter('Hi there Nikkie ', ' ', 0));
+
+//Another solution:
+
+// function stringSplitter(str, splitter) {
+    
+//   if(str.indexOf(splitter) === -1){
+//       return [str];
+//   }else{
+//       let temp = str.indexOf(splitter);
+//       let tempString = str.slice(0,temp);
+//       return [tempString, ...stringSplitter(str.slice(temp+1),splitter) ]
+//   }
+// }
