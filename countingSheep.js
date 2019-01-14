dc01ef7bc993708c1480841f33b2004320acea71
James Bell <Noxachron@gmail.com>
2019-01-14 11:29:31 -0500

    counting sheep drill done

---
+++ countingSheep.js
@@ -0,0 +1,24 @@
+/* what is the input:
+a number (of sheep)
+
+what is the output:
+a console log of the number( of sheep) with the message "Another sheep jump over the fence"
+
+what is the input to each recursive call:
+one less sheep (number -1 ) 
+what is the output to each recursive call:
+the number of sheep left, with the message
+
+ */
+
+function sheepCounting(number){
+    //base case
+    if(number===0){
+        return ;
+    }
+    //recursive case
+    console.log(number, "Another sheep jump over the fence");
+    sheepCounting(number -1);
+}
+
+sheepCounting(5);
