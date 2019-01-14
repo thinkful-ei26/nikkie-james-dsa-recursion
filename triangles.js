7a4aa0854bd430bd8750982f96622463deeda7bf
James Bell <Noxachron@gmail.com>
2019-01-14 14:17:34 -0500

    added triangles drill

--- triangles.js
+++ triangles.js
@@ -1,37 +1,21 @@
-
-
-//[1 , 0 ,0 ]
-//no sec layer
-
-
-// [0, *, 0] add new layer, ++ this layer
-// filter (  if === ?)//to become a star or space
-// pass filter = star
-// no pass = space
-
-//    if it is + or - 1 single number (condition to pass)
-
-// [ X, X , X ] filter number is 2 ( any star previous layer +- 1?)
-// star is at index 1
-
-//[  0 pass => *, 1 ! +- 1 fail => space, 2-1=star => star ]
-//[ *, 0 , *]
-
-//in each future recursive call we compare (the previous layer: var)  && (condition: constant doesnt change)
-
 /*
-What is the input to the program?
-some number to count up to for how many triangles to make.
-->> Num
+What is the input to the program
+the number to analyze up to
 What is the output of the program
-the what purpose?
- how many the number of stars on each side? a number! this many.
-
- how we calculate that?
- filter each layer against (condition): +-1 prev layer
-
+the numbers that fit the formula
 What is the input to each recursive calls
-the previous layer +1 bumped up by one( now it's not the bottom layer)
-the condition is 
+the next additive amount
 What is the output of each recursive calls
+the next number in the sequence
 */
+              
+function triangles(number){
+    if(number<=0){
+        return 0;
+    }
+    return number + triangles(number - 1);
+
+}
+console.log(triangles(6));
+//10
+//1, 3, 6, 10, 15, 21, 28, 36, 45, 55
